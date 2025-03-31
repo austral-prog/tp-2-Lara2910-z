d70d59dc9ad1f2d3a653cdfb3c2d51ae4221bba9
Lara2910-z <lzarantonello@mail.austral.edu.ar>
2025-03-31 13:10:10 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    #Ejercicio 1
+    full_name = f"{first_name} {last_name}"
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    full_name_tab = f"\t{full_name.lower()}"
+    print(full_name_tab)
