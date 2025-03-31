39db40b3f67eeee7be3a6fbac5fd7c2532766f23
Lara2910-z <lzarantonello@mail.austral.edu.ar>
2025-03-31 13:10:54 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,8 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    #Ejercicio 2
+    menor = (f"The result of {x} comes first in the dictionary than {y} is {x < y}.")
+    print(menor)
+    mayor = (f"The result of {y} comes first in the dictionary than {x} is {x > y}.")
+    print(mayor)
